b6cc5f3fb2bcb487aa746662d5a599b4ac807bcb
Jjiroos <cloclo2412@gmail.com>
2024-06-11 23:51:56 +0200

    minor changes

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"isen.p18.demineur"}];updateSearchResults();
--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"isen.p18.demineur","l":"Gui"}];updateSearchResults();
